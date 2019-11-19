8e490cb538ba67cc828bf7793c7ebded7414e33d
4superu <obarashigeharu@gmail.com>
2019-11-19 19:41:49 +0900

    fix HOG

--- Question_61_70/myanswers/myans68.py
+++ Question_61_70/myanswers/myans68.py
@@ -13,7 +13,8 @@ def HOG(gray):
 
         gray = np.pad(gray, (1, 1), 'edge')
         gx = gray[1:Ver+1, 2:] - gray[1:Ver+1, :Hor]
-        gy = gray[2:, 1:Hor+1] - gray[:Ver, 1:Hor+1]
+        gy = gray[:Ver, 1:Hor+1] - gray[2:, 1:Hor+1]
+        #gy = gray[2:, 1:Hor+1] - gray[:Ver, 1:Hor+1]
         # keep from zero-dividing
         gx[gx == 0] = 1e-6
 
@@ -24,7 +25,7 @@ def HOG(gray):
         ang = np.arctan(gy / gx)
 
         # arctanは返り値(-2/pi, 2/pi)なので，負の値はpi回転
-        ang[ang < 0] += np.pi
+        ang[ang < 0] = np.pi + ang[ang < 0]
 
         return mag, ang
 
@@ -36,7 +37,7 @@ def HOG(gray):
             low = (np.pi * i) / 9
             high = (np.pi * (i + 1)) / 9
 
-            quantized_ang[np.where((ang >= low) & (ang < high))] = i + 1
+            quantized_ang[np.where((ang >= low) & (ang < high))] = i
 
         return quantized_ang
 
@@ -45,24 +46,30 @@ def HOG(gray):
 
         CELL_H = Hor // sell
         CELL_V = Ver // sell
+        N = sell
         hist = np.zeros((CELL_V, CELL_H, 9), dtype=np.float32)
-
+        for y in range(CELL_V):
+            for x in range(CELL_H):
+                for j in range(N):
+                    for i in range(N):
+                        hist[y, x, ang[y * N + j, x * N + i]] += mag[y * N + j, x * N + i]
+        
+        '''
         for x in range(Hor):
             hx = x // sell
             for y in range(Ver):
                 hy = y // sell
 
                 hist[hy, hx, ang[y, x]-1] += mag[y, x]
-
+        '''
         return hist
 
     def QuantizationHistogram(histogram, epsilon=1):
         Ver, Hor, Index = histogram.shape
 
-
         for x in range(Hor):
             for y in range(Ver):
-                histogram[y, x] = histogram[y, x] / np.sqrt(np.sum(histogram[max(y-1, 0) : min(y+2, Ver), max(x-1, 0) : min(x+2, Hor)]) + epsilon)
+                histogram[y, x] = histogram[y, x] / np.sqrt(np.sum(histogram[max(y-1, 0) : min(y+2, Ver), max(x-1, 0) : min(x+2, Hor)] ** 2) + epsilon)
 
         return hist
 
@@ -96,7 +103,8 @@ def DrawHOG(hist, gray, sell=8):
             y1 = iy
             y2 = iy
 
-            h = histogram[y, x] / np.sum(histogram)
+            h = histogram[y, x] / np.sum(histogram[y, x])
+            h /= h.max()
 
             for ind in range(Index):
                 angle = (ind * 20 + 10) / 180 * np.pi
@@ -106,12 +114,11 @@ def DrawHOG(hist, gray, sell=8):
                 ly = int(np.cos(angle) * (x2 - ix) - np.cos(angle) * (y2 - iy) + iy)
 
                 # color is HOG value
-                c = int(255. * h[..., ind])
+                c = int(255. * h[ind])
 
                 # draw line
                 cv2.line(gray, (lx, ly), (rx, ry), c, thickness=1)
     return gray
-
 
 img = cv2.imread("../imori.jpg").astype(np.float)
 gray = BGRtoGRAY(img)
@@ -119,6 +126,7 @@ gray = BGRtoGRAY(img)
 histogram = HOG(gray)
 
 result = DrawHOG(histogram, gray)
+result = result.astype(np.uint8)
 
 cv2.imwrite("myans_69.jpg", result)
 cv2.imshow("result", result)
